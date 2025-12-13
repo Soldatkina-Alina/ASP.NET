6cce7ceb500da4a292c8ac6335146c68470c8fbe
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2025-12-13 21:41:08 +0300

    [Дз-1] Реализованы Create/Delete/Update методы в EmployeesController

--- Homeworks/Base/src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
+++ Homeworks/Base/src/PromoCodeFactory.WebHost/Controllers/EmployeesController.cs
@@ -17,10 +17,101 @@ namespace PromoCodeFactory.WebHost.Controllers
     public class EmployeesController : ControllerBase
     {
         private readonly IRepository<Employee> _employeeRepository;
+        private readonly IRepository<Role> _roleRepository;
 
-        public EmployeesController(IRepository<Employee> employeeRepository)
+        public EmployeesController(IRepository<Employee> employeeRepository, IRepository<Role> roleRepository)
         {
             _employeeRepository = employeeRepository;
+            _roleRepository = roleRepository;
+        }
+
+        /// <summary>
+        /// Создать нового сотрудника
+        /// </summary>
+        [HttpPost]
+        public async Task<ActionResult<EmployeeResponse>> CreateEmployeeAsync([FromBody] EmployeeCreateRequest request)
+        {
+            var roles = (await _roleRepository.GetAllAsync()).Where(role => request.RoleIds.Contains(role.Id)).ToList();
+            var employee = new Employee
+            {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Roles = roles,
+                AppliedPromocodesCount = request.AppliedPromocodesCount,
+            };
+
+            await _employeeRepository.AddAsync(employee);
+
+            var response = new EmployeeResponse
+            {
+                Id = employee.Id,
+                FullName = employee.FullName,
+                Email = employee.Email,
+                Roles = employee.Roles.Select(x => new RoleItemResponse
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description
+                }).ToList(),
+                AppliedPromocodesCount = employee.AppliedPromocodesCount
+            };
+
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Обновить данные сотрудника
+        /// </summary>
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<EmployeeResponse>> UpdateEmployeeAsync(Guid id, [FromBody] EmployeeUpdateRequest request)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.FirstName = request.FirstName;
+            employee.LastName = request.LastName;
+            employee.Email = request.Email;
+            employee.Roles = (await _roleRepository.GetAllAsync()).Where(role => request.RoleIds.Contains(role.Id)).ToList();
+
+            await _employeeRepository.UpdateAsync(employee);
+
+            var response = new EmployeeResponse
+            {
+                Id = employee.Id,
+                FullName = employee.FullName,
+                Email = employee.Email,
+                Roles = employee.Roles.Select(x => new RoleItemResponse
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description
+                }).ToList(),
+                AppliedPromocodesCount = employee.AppliedPromocodesCount
+            };
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Удалить сотрудника
+        /// </summary>
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteEmployeeAsync(Guid id)
+        {
+            var employee = await _employeeRepository.GetByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            await _employeeRepository.DeleteAsync(id);
+
+            return NoContent();
         }
 
         /// <summary>
@@ -61,6 +152,7 @@ namespace PromoCodeFactory.WebHost.Controllers
                 Email = employee.Email,
                 Roles = employee.Roles.Select(x => new RoleItemResponse()
                 {
+                    Id = x.Id,
                     Name = x.Name,
                     Description = x.Description
                 }).ToList(),
