72bc3b06284719c2fb689a9ff9abe0e75513f8b2
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2026-01-07 14:16:12 +0300

    Изменены классы доменной модели

--- Homeworks/EF/src/PromoCodeFactory.Core/Domain/Administration/Role.cs
+++ Homeworks/EF/src/PromoCodeFactory.Core/Domain/Administration/Role.cs
@@ -3,8 +3,7 @@ using System;
 
 namespace PromoCodeFactory.Core.Domain.Administration
 {
-    public class Role
-        : BaseEntity
+    public class Role: BaseEntity
     {
         public string Name { get; set; }
 
