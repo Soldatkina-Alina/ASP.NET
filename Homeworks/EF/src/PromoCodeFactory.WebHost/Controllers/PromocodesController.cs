ce27a79e0191e3577d40895235bf915064c42acc
Soldatkina-Alina <alinasoldatkina2014@gmail.com>
2026-01-10 16:57:18 +0300

    Реализован контроллер для промокодов

--- Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
+++ Homeworks/EF/src/PromoCodeFactory.WebHost/Controllers/PromocodesController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using PromoCodeFactory.WebHost.Models;
 
 namespace PromoCodeFactory.WebHost.Controllers
@@ -14,15 +17,35 @@ namespace PromoCodeFactory.WebHost.Controllers
     public class PromocodesController
         : ControllerBase
     {
+        private readonly IRepository<PromoCode> _promoCodeRepository;
+        private readonly IRepository<Customer> _customerRepository;
+        private readonly IRepository<Preference> _preferenceRepository;
+
+        public PromocodesController(IRepository<PromoCode> promoCodeRepository, IRepository<Customer> customerRepository, IRepository<Preference> preferenceRepository)
+        {
+            _promoCodeRepository = promoCodeRepository;
+            _customerRepository = customerRepository;
+            _preferenceRepository = preferenceRepository;
+        }
         /// <summary>
         /// Получить все промокоды
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public Task<ActionResult<List<PromoCodeShortResponse>>> GetPromocodesAsync()
+        public async Task<ActionResult<List<PromoCodeShortResponse>>> GetPromocodesAsync()
         {
-            //TODO: Получить все промокоды 
-            throw new NotImplementedException();
+            var promoCodes = await _promoCodeRepository.GetAllAsync();
+            var response = promoCodes.Select(pc => new PromoCodeShortResponse
+            {
+                Id = pc.Id,
+                Code = pc.Code,
+                ServiceInfo = pc.ServiceInfo,
+                BeginDate = pc.BeginDate.ToString("dd-MM-yyyy"),
+                EndDate = pc.EndDate.ToString("dd-MM-yyyy"),
+                PartnerName = pc.PartnerName
+            }).ToList();
+
+            return Ok(response);
         }
 
         /// <summary>
@@ -30,10 +53,44 @@ namespace PromoCodeFactory.WebHost.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpPost]
-        public Task<IActionResult> GivePromoCodesToCustomersWithPreferenceAsync(GivePromoCodeRequest request)
+        public async Task<IActionResult> GivePromoCodesToCustomersWithPreferenceAsync(GivePromoCodeRequest request)
         {
-            //TODO: Создать промокод и выдать его клиентам с указанным предпочтением
-            throw new NotImplementedException();
+            var preference = await _preferenceRepository.GetAllAsync(r=> r.Name == request.Preference);
+            var targetPreference = preference.FirstOrDefault();
+
+            if (targetPreference == null)
+            {
+                return NotFound("Preference not found");
+            }
+
+            // Найти клиентов с этим предпочтением
+            var customerPreferences = await _customerRepository.GetAllAsync(c => c.Preferences.Any(cp => cp.PreferenceId == targetPreference.Id));
+            var customersWithPreference = customerPreferences.ToList();
+
+            if (!customersWithPreference.Any())
+            {
+                return BadRequest("No customers with this preference");
+            }
+
+            // Создать и сохранить промокоды для каждого клиента
+            var promoCodesToCreate = customersWithPreference.Select(customer => new PromoCode
+            {
+                Id = Guid.NewGuid(),
+                Code = request.PromoCode,
+                ServiceInfo = request.ServiceInfo,
+                BeginDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(1),
+                PartnerName = request.PartnerName,
+                Customer = customer,
+                Preference = targetPreference
+            }).ToList();
+
+            foreach (var promoCode in promoCodesToCreate)
+            {
+                await _promoCodeRepository.CreateAsync(promoCode);
+            }
+
+            return Ok();
         }
     }
 }
